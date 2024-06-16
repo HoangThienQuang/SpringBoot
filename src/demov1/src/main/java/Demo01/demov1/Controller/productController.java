6c05d6bb9385b22369006f1c165a5823d5835f31
HoangThienQuang <quang2.11.1999@gmail.com>
2024-06-17 01:03:41 +0700

    add update product API but not done yet

--- src/demov1/src/main/java/Demo01/demov1/Controller/productController.java
+++ src/demov1/src/main/java/Demo01/demov1/Controller/productController.java
@@ -3,13 +3,13 @@ package Demo01.demov1.Controller;
 import Demo01.demov1.Models.Product;
 import Demo01.demov1.Models.responseObject;
 import Demo01.demov1.Repositories.productRepo;
+import Demo01.demov1.data.database;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,10 +17,12 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path = "/api/v1/Products")
 public class productController {
+    private static final Logger logger = LoggerFactory.getLogger(productController.class);
 
     @Autowired
     private productRepo repository;
 
+    //Get all product -----------------------------------------------------------------------------------
     @GetMapping("")
     //
     List<Product> getAllProducts()
@@ -28,6 +30,7 @@ public class productController {
         return  repository.findAll();
     }
 
+    // Get product by id --------------------------------------------------------------------------------
     @GetMapping("/{id}")
     ResponseEntity<responseObject> findById (@PathVariable Long id)
     {
@@ -39,5 +42,39 @@ public class productController {
                 ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 new responseObject("false","Can not found product with id: " + id, "")
                 );
+    }
+
+    // Add new product --- --------------------------------------------------------------------------------
+    @PostMapping("/insert")
+    ResponseEntity<responseObject> insertProduct(@RequestBody Product newproduct)
+    {
+        List<Product> foundProduct = repository.findByProductName(newproduct.getProductName().trim());
+        return !foundProduct.isEmpty() ?
+            ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                    new responseObject("Fail", "Product already exit", "")
+            ):
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new responseObject("ok", "Insert successful", repository.save(newproduct))
+                );
+    }
+
+    // upsert - Update existed product if product doesn't exist then insert new
+    @PutMapping("/{id}")
+    ResponseEntity<responseObject> updateProduct(@RequestBody Product newProduct, @PathVariable Long id)
+    {
+        Product updateProduct = repository.findById(id)
+                .map(product -> {
+                    product.setProductName(newProduct.getProductName());
+                    product.setPrice(newProduct.getPrice());
+                    product.setDevyear(newProduct.getDevyear());
+                    product.setUrl(newProduct.getUrl());
+                    return repository.save(product);
+                }).orElseGet(()->
+                {
+                    return repository.save(newProduct);
+                });
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new responseObject("ok", "Update successful", updateProduct)
+        );
     }
 }
--- src/demov1/src/main/java/Demo01/demov1/Repositories/productRepo.java
+++ src/demov1/src/main/java/Demo01/demov1/Repositories/productRepo.java
@@ -3,5 +3,8 @@ package Demo01.demov1.Repositories;
 import Demo01.demov1.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface productRepo extends JpaRepository<Product, Long> {
+    List<Product> findByProductName(String productName);
 }
